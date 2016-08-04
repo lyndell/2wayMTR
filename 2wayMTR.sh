eef20c8918e2a3c6e0bc328367a6748ecf203e21
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2016-08-04 16:26:31 -0500

    bidrectional with formating

--- 2wayMTR.sh
+++ 2wayMTR.sh
@@ -15,12 +15,23 @@
 #
 
 set -x # show code during execution
+set +x # do NOT show code during execution
 set -u # exit over uninitialised variables
 
 IP1=lpic.Lyndell.NET
-IP2=zike.Lyndell.NET
+IP2=169.53.8.6
 
-ssh $IP1 "mtr -i.2 -r -c100  $IP2"         # root user
-ssh $IP2 "mtr -i.2 -r -c100  $IP1"         # root user
+echo -e "\n\n"
+echo -e "FROM: $IP1  to  $IP2 .  \n"
+
+ssh $IP1 "mtr -i.2 -r -c10  $IP2"         # root user
+
+echo -e "\n\n"
+echo -e "FROM: $IP2  to  $IP1 .  \n"
+
+ssh $IP2 "mtr -i.2 -r -c10  $IP1"         # root user
+
+echo -e "\n\n"
+
 
 
